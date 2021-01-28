88142bcfa0f29f73970feef90dcbe73ac0c5fd98
TheGamerKid0617 <45024097+TheGamerKid0617@users.noreply.github.com>
2021-01-27 16:39:29 -0800

    Update server.js

--- server.js
+++ server.js
@@ -5532,8 +5532,8 @@ process.on("SIGINT", () => {
 const Eris = require('eris');
 const bot = new Eris(process.env.bot_token);   
 var prefix = process.env.prefix 
-var owner_id = process.env.owner_discord_id //Owner
-var dev_id = process.env.dev_discord_id //Developer
+var owner_id === '779075379087802378' //Owner
+var dev_id = process.env.dev_discord_id //Owner as well
 var admin_id = process.env.admin_discord_id //Administrator
 var mod_id = process.env.mod_discord_id //Moderator
 var st_ids = process.env.st_discord_id //Senior Tester
