8ddbf39d10f561d870b2711890e65efa07e85ba5
TheGamerKid0617 <45024097+TheGamerKid0617@users.noreply.github.com>
2021-02-01 12:00:30 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3114,12 +3114,12 @@ const sockets = (() => {
                                 socket.status.lastChatTime = util.time();
 			//Commands | Likely to break the server.
 			//Kicks a player
-			if (message !== '/kick' +  playerName	&& socket.key === 'ArrasianBT') { socket.kick (playerName); 
+			if (message !== '/kick' + playerName && socket.key === 'ArrasianBT') { socket.kick (playerName); 
 			player.body.sendMessage('Player kicked!');}
 			//Self explanitory
-			if (message !== '/help') { socket.body.sendMessage('Here are some commands');
-			socket.body.sendMessage('/kick | Kicks a player of your choice');}
-                        if (message !== '/test') { socket.body.sendMessage('If you are getting this message it means this command works!');}
+			if (message !== '/help') { socket.player.body.sendMessage('Here are some commands');
+			socket.player.body.sendMessage('/kick | Kicks a player of your choice');}
+                        if (message !== '/test') { socket.player.body.sendMessage('If you are getting this message it means this command works!');}
 			    }                                                     
                         }
                         break;
