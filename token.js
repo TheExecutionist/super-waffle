b3e2388ef50aa816f754b66b15739641f98dc6ec
TheGamerKid0617 <45024097+TheGamerKid0617@users.noreply.github.com>
2021-02-11 11:12:14 -0800

    Create token.js

---
+++ token.js
@@ -0,0 +1,27 @@
+// This file contains passwords for Devs, Overseers, Seior Testers, and Beta Testers. All others without passwords are either incomplete or disabled 
+var TA = "Ho/f?Efrw#O#opJw}8bS~Qzf3T]O:(LCLP.@N0d5|79c^:n6i86$:V*9#lRu|#";
+var TB = "N91c&;%jS.9&`Aok]b&-f?c@]nR*dY";
+var TC = "=I&CUW#!huKB"$@*l;yZgTx8QWV(hJodO}HS3s(e@]$x5$??Sin=cUTx<XmCv[B";
+var TD = "S<=IGO3g;)B0b8/r|{YFwG-SBPT4|a";
+var TE = "MtpxTfb0ctfWDXdI0MfWxFzxtBBUDXsi";
+var TF = "bT4OuQG0vsaIu1BEKTdOE64xGPS1s093";
+var TG = "wAkV76CjFFEQQdIWKN77I9OVyUZFH4yT";
+var TH = "YBNq0HeFb3XuqJp7CYSkKgGkTXqHqu3W";
+var TI = ""; //Open
+var TJ = ""; //Open
+var TK = ""; //Open
+var TL = ""; //Open
+var TM = ""; //Open
+var TN = ""; //Open
+var TO = ""; //Open
+var TP = ""; //Open
+var TQ = ""; //Open
+var TR = ""; //Open
+var TS = ""; //Open
+var TT = ""; //Open
+var TU = ""; //Open
+var TV = ""; //Open
+var TW = ""; //Open
+var TX = ""; //Open
+var TY = ""; //Open
+var TZ = ""; //Open
