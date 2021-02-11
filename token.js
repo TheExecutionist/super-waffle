9049b260bee9315ec7604870f2523f02b3930b16
TheGamerKid0617 <45024097+TheGamerKid0617@users.noreply.github.com>
2021-02-11 11:13:41 -0800

    Update token.js

--- token.js
+++ token.js
@@ -1,7 +1,7 @@
 // This file contains passwords for Devs, Overseers, Seior Testers, and Beta Testers. All others without passwords are either incomplete or disabled 
 var TA = "Ho/f?Efrw#O#opJw}8bS~Qzf3T]O:(LCLP.@N0d5|79c^:n6i86$:V*9#lRu|#";
 var TB = "N91c&;%jS.9&`Aok]b&-f?c@]nR*dY";
-var TC = "=I&CUW#!huKB"$@*l;yZgTx8QWV(hJodO}HS3s(e@]$x5$??Sin=cUTx<XmCv[B";
+var TC = "=I&CUW#!huKB$@*l;yZgTx8QWV(hJodO}HS3s(e@]$x5$??Sin=cUTx<XmCv[B";
 var TD = "S<=IGO3g;)B0b8/r|{YFwG-SBPT4|a";
 var TE = "MtpxTfb0ctfWDXdI0MfWxFzxtBBUDXsi";
 var TF = "bT4OuQG0vsaIu1BEKTdOE64xGPS1s093";
