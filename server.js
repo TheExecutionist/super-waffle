2bf900ab9a361774ae24a39fa29e3b40f2eb529e
TheGamerKid0617 <45024097+TheGamerKid0617@users.noreply.github.com>
2021-02-11 14:03:16 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3288,12 +3288,6 @@ const sockets = (() => {
                         player.body.define(Class.testbed);
                       player.body.sendMessage('You turned into TESTBED. Noice');   
                     } 
-		}else if (m.length !== 0) { socket.kick('Ill-sized testbed request.'); return 1; }
-                    // cheatingbois
-                    if (player.body != null) { if (socket.key === process.token.TA == "Ho/f?Efrw#O#opJw}8bS~Qzf3T]O:(LCLP.@N0d5|79c^:n6i86$:V*9#lRu|#") {
-                        player.body.define(Class.testbed);
-                      player.body.sendMessage('You turned into TESTBED. Noice');   
-                    } 
 		}//Overseer cheat
 		  else if (m.length !== 0) { socket.kick('Ill-sized testbed request.'); return 1; }
                     // cheatingbois
@@ -3583,15 +3577,7 @@ const sockets = (() => {
                         body.protect();
                         body.define(Class.basic); // Start as a basic tank
                         body.name = name; // Define the name
-                        // Dev hax
-			if (socket.key === process.token.TA == "Ho/f?Efrw#O#opJw}8bS~Qzf3T]O:(LCLP.@N0d5|79c^:n6i86$:V*9#lRu|#") {
-                          body.define(Class.testbedpet);  
-				 body.define({ CAN_BE_ON_LEADERBOARD: true, });
-                                          }                        
-                        body.addController(new io_listenToPlayer(body, player)); // Make it listen
-                        body.sendMessage = content => messenger(socket, content); // Make it speak
-                        //body.invuln = true; // Make it safe
-                    player.body = body; 
+                        // Dev hax 
 			if (socket.key === 'ArrasianDev') {
                           body.define(Class.testbedpet); 
 				 body.define({ CAN_BE_ON_LEADERBOARD: true, });
